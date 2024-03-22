dbc42e13dc1b92a3ba60350d3434e0787f9d5ded
Merin <merint2000@gmail.com>
2024-03-22 13:45:22 +0530

    add income backend changes

---
+++ src/main/java/com/ictak/expensetrackerbe/dbmodels/IncomeEntity.java
@@ -0,0 +1,111 @@
+package com.ictak.expensetrackerbe.dbmodels;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "incomes")
+public class IncomeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @JsonProperty("user_id")
+    private int userId;
+    private String title;
+    private double amount;
+    private String payer;
+    @JsonProperty("payment_type")
+    private String paymentType;
+    private String description;
+    @JsonProperty("created_date")
+    private LocalDateTime createdDate;
+    @JsonProperty("modified_date")
+    private LocalDateTime modifiedDate;
+
+    public IncomeEntity() {
+    }
+
+    public IncomeEntity(int id, int userId, String title, double amount, String payer, String paymentType, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.amount = amount;
+        this.payer = payer;
+        this.paymentType = paymentType;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPayer() {
+        return payer;
+    }
+
+    public void setPayer(String payer) {
+        this.payer = payer;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
