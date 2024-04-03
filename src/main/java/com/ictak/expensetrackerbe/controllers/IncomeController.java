0f978afca99476baa01a97bb800ba03d520c050e
Abinithya <abinithya.samdas@gmail.com>
2024-04-03 10:10:57 +0530

    Insight changes

--- src/main/java/com/ictak/expensetrackerbe/controllers/IncomeController.java
+++ src/main/java/com/ictak/expensetrackerbe/controllers/IncomeController.java
@@ -51,26 +51,4 @@ public class IncomeController {
         }
         return ResponseEntity.ok(response);
     }
-
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/insightsin")
-    public ResponseEntity<Map<String, Object>> getIncomes(@RequestParam int userId) {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            List<IncomeEntity> result= incomeRepository.getIncomes(userId);
-            if (result != null) {
-                response.put("status", "success");
-                response.put("code", 200);
-                response.put("insightsin", result);
-            } else {
-                response.put("code", 404);
-                response.put("insightsin", new ArrayList<>());
-            }
-        } catch (Exception e) {
-            response.put("status", "error");
-            response.put("code", 500);
-            response.put("message", e.getMessage());
-        }
-        return ResponseEntity.ok(response);
-    }
 }
